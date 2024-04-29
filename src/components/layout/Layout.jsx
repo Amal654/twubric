67a0ef3011ba5e8022769b1984d83237193fe7fd
Amal654 <91201286+Amal654@users.noreply.github.com>
2024-04-29 14:55:26 +0530

    ui fix

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <div className="bg-neutral-50">
+      <div className="bg-neutral-100">
         <div className="container flex justify-center flex-col items-center">
           {children}
         </div>
